8662795309c7c4cb829b6155474d21e47255b45e
OdaiJawabreh <odai.jawabreh.oj@gmail.com>
2021-07-11 14:00:33 +0300

    p.c Q#1&2&3&4&5

--- main.js
+++ main.js
@@ -24,11 +24,27 @@ const orderedPlanets1 = ["Mercury", "Venus", "Earth", "Jupiter"];
 orderedPlanets1[orderedPlanets1.length]  ="mars"
 const unorderedPlanets1 = ["Mars", "Earth", "Mercury"]
 unorderedPlanets1.push("mars")
-const animals = ["Cat", "Dog", "Dolphin", "Squirrel"];
-animals[0]="Koala"
+const animals1 = ["Cat", "Dog", "Dolphin", "Squirrel"];
+animals1[0]="Koala"
 //q#4
 const reptiles = ["Snake", "Lizard", "Turtle"];
 reptiles.push("Dinosaur ")
+reptiles.shift(0)
+const fish = ["Swordfish", "Clownfish", "Shark"];
+fish.unshift("Goldfish")
+fish.pop(3)
+//q#5
+const array = [1, 9, 9, 3];
+array.join("");
+const array1 = ["John", "Doe", "The Third"];
+array1.join(" ")
+array3 = [1, 2, 3, 4];
+array3.reverse()
+
+
+
+
+
 
 
 
