7ca1e22c2c19b9c5fd6c37f3e68ced0109298e13
OdaiJawabreh <odai.jawabreh.oj@gmail.com>
2021-07-11 13:39:43 +0300

    p Q#1&2&3

---
+++ main.js
@@ -0,0 +1,50 @@
+console.log(arrowTitle)
+/* START CODE UNDER THIS LINE */
+console.log("W02D01")
+//pulse check
+//q#1
+const myFavoriteColor = ["red","Blue","green"]
+const negativeNumbers  = [-1,-2 ,-3 ,-4 ,-5]
+const food = ["mansaf","Brosted","Makmoura"]
+const numbers = [
+    [1,3,5],
+    [2,4,6]
+]
+//q#2
+const orderedPlanets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter"];
+orderedPlanets[3]
+const unorderedPlanets = ["Mars", "Earth", "Mercury", "Jupiter", "Venus"];
+orderedPlanets[0]
+const animals = ["Cat", "Dog", "Dolphin", "Squirrel", "Koala"];
+animals[(orderedPlanets.length)-1]
+//q#3
+
+
+const orderedPlanets1 = ["Mercury", "Venus", "Earth", "Jupiter"];
+orderedPlanets1[orderedPlanets1.length]  ="mars"
+const unorderedPlanets1 = ["Mars", "Earth", "Mercury"]
+unorderedPlanets1.push("mars")
+const animals = ["Cat", "Dog", "Dolphin", "Squirrel"];
+animals[0]="Koala"
+//q#4
+const reptiles = ["Snake", "Lizard", "Turtle"];
+reptiles.push("Dinosaur ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
